fd269e73692b2420bd8e412ca5f0c06ef1a44e59
John Papa <john@johnpapa.net>
2014-12-28 09:44:21 -0500

    now injecting CSS along with wiredep for js into the html

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -91,6 +91,15 @@ gulp.task('wiredep', function() {
         .pipe(gulp.dest(config.client));
 });
 
+gulp.task('inject', ['wiredep', 'styles', 'templatecache'], function() {
+    log('Wire up css css into the html, after files are ready');
+
+    return gulp
+        .src(config.index)
+        .pipe($.inject(gulp.src(config.css)))
+        .pipe(gulp.dest(config.client));
+});
+
 /**
  * Copy fonts
  * @return {Stream}
