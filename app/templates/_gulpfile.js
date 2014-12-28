5a31a517e069e4cff37e9b6b36c5f066512f62ac
John Papa <john@johnpapa.net>
2014-12-28 15:02:51 -0500

    made child process log when it shuts down. removed callback as api has changed for fork

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -508,7 +508,7 @@ function startTests(singleRun, done) {
         var savedEnv = process.env;
         savedEnv.NODE_ENV = 'dev';
         savedEnv.PORT = 8888;
-        child = fork(config.nodeServer, childProcessCompleted);
+        child = fork(config.nodeServer);
     } else {
         excludeFiles.push(config.midwaySpecs);
     }
@@ -521,16 +521,9 @@ function startTests(singleRun, done) {
 
     ////////////////
 
-    function childProcessCompleted(error, stdout, stderr) {
-        log('stdout: ' + stdout);
-        log('stderr: ' + stderr);
-        if (error !== null) {
-            log('exec error: ' + error);
-        }
-    }
-
     function karmaCompleted() {
         if (child) {
+            log('shutting down the child process');
             child.kill();
         }
         done();
