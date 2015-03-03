d6e37384560a6867c2dc6cb4e0a609af2c611b27
John Papa <john@johnpapa.net>
2015-03-02 23:49:47 -0700

    added data and routes for the server. added rootScope for the splash screen

---
+++ app/templates/src/server/_routes.js
@@ -0,0 +1,28 @@
+var router = require('express').Router();
+var four0four = require('./utils/404')();
+var data = require('./data');
+
+router.get('/people', getPeople);
+router.get('/person/:id', getPerson);
+router.get('/*', four0four.notFoundMiddleware);
+
+module.exports = router;
+
+//////////////
+
+function getPeople(req, res, next) {
+    res.status(200).send(data.people);
+}
+
+function getPerson(req, res, next) {
+    var id = +req.params.id;
+    var person = data.people.filter(function(p) {
+        return p.id === id;
+    })[0];
+
+    if (person) {
+        res.status(200).send(person);
+    } else {
+        four0four.send404(req, res, 'person ' + id + ' not found');
+    }
+}
